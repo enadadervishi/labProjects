dd07ed805e55edbe15138ec4a114cbd8ee49b904
enadadervishi <enada.dervishi@studenti.unimi.it>
2023-05-02 07:27:21 +0200

    Same + gradle depencecies for using proto GPRC blablabla [problems still in Broadcasting & now in JSON writing and reading & moreover warnings (look WARNING.png)]

---
+++ src/main/java/lab3/studentStats/Student.java
@@ -0,0 +1,43 @@
+package lab3.studentStats;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class Student {
+
+    public static void main(String[] args) throws IOException {
+
+        Socket s = new Socket("localhost", 9999);
+
+        /**
+        Student actor =
+                Student.newBuilder()
+                        .setName("Christian")
+                        .setSurname("Bale")
+                        .setSex(Actor.Sex.MALE)
+                        .addMovie(Actor.Movie.newBuilder()
+                                .setTitle("The Prestige")
+                                .setYear(2006))
+                        .addMovie(Actor.Movie.newBuilder()
+                                .setTitle("The Dark Knight")
+                                .setYear(2008))
+                        .build();
+
+        actor.writeTo(s.getOutputStream());
+
+        s.close();
+        */
+
+
+
+    }
+
+
+
+
+
+
+}
+
+
+
